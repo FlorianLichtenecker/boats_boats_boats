1308e5f6a8a995f738d594f75741e1416dcd22b0
Florian Lichtenecker <florian.lichtenecker@gmail.com>
2023-08-23 11:41:38 +0200

    updated show boat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_100833) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_151352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_100833) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "boat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_reviews_on_boat_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -94,4 +103,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_100833) do
   add_foreign_key "booking_requests", "users"
   add_foreign_key "bookings", "boats"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "boats"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,30 +4,28 @@
 User.destroy_all
 Boat.destroy_all
 
-5.times do
-  # Create a user
-  user = User.create!(
-    email: Faker::Internet.unique.email,
-    password: 'password123' # For simplicity, using a common password for all seeded users
-  )
-
-  # Create 2 boats for the user
-  2.times do
-    Boat.create!(
-      name: Faker::Vehicle.unique.make,
-      length: Faker::Number.decimal(l_digits: 2, r_digits: 2),
-      width: Faker::Number.decimal(l_digits: 2, r_digits: 2),
-      height: Faker::Number.decimal(l_digits: 2, r_digits: 2),
-      user: user
-    )
-  end
-end
-
-puts "Seeded #{User.count} users and #{Boat.count} boats."
-
 User.create!(
   email: "123@123.com",
   password: "123123"
 )
 
 puts "Log-in user (123123@123.com, 123123) created."
+
+user = User.first
+image_names = ['boat_1.jpg', 'boat_2.jpg', 'boat_3.jpg']
+
+3.times do |index|
+  boat = Boat.create!(
+    name: Faker::Vehicle.unique.make,
+    length: Faker::Number.decimal(l_digits: 2, r_digits: 2),
+    width: Faker::Number.decimal(l_digits: 2, r_digits: 2),
+    height: Faker::Number.decimal(l_digits: 2, r_digits: 2),
+    user: user
+  )
+
+  # Attach an image from app/assets/images directory
+  boat_image_path = Rails.root.join('../boats_boats_boats/app/assets/images', image_names[index])
+  boat.images.attach(io: File.open(boat_image_path), filename: image_names[index], content_type: 'image/jpg')
+end
+
+puts "Seeded #{Boat.count} boats."
