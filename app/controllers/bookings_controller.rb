08875a5d895b515dd2382fae6c5ef62db6ca8d55
Florian Lichtenecker <florian.lichtenecker@gmail.com>
2023-08-23 15:36:05 +0200

    bookings added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,15 @@ class BookingsController < ApplicationController
   def create
     date_range = booking_params[:date_range].split(" to ")
     @booking = Booking.new(start_date: Date.parse(date_range[0]), end_date: Date.parse(date_range[1]), status: "open")
-    @booking.save
+    @booking.user = current_user
+    @booking.boat = Boat.find(params[:boat_id])
+    if @booking.save
+      flash[:notice] = 'Booking request sent'
+      redirect_to boat_path(@booking.boat)
+    else
+      # Handle the error
+      render 'boats/show'
+    end
   end
 
   private
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
+  attr_accessor :date_range
+  
   belongs_to :user
   belongs_to :boat
 end
