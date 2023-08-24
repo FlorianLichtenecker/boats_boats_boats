814eb74055737a1eb6a375270ed0cf73978e11aa
Mateo Campins <mateo.campins@gmail.com>
2023-08-24 12:53:04 +0200

    reviews improved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def render_stars(rating)
+    return unless rating.present? # Skip rendering if rating is nil or blank
+
+    content_tag(:span, class: 'stars') do
+      rating.times do
+        concat content_tag(:i, '', class: 'fas fa-star')
+      end
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_123112) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_102312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +71,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_123112) do
     t.bigint "boat_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["boat_id"], name: "index_reviews_on_boat_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -92,4 +94,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_123112) do
   add_foreign_key "bookings", "boats"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "boats"
+  add_foreign_key "reviews", "users"
 end
